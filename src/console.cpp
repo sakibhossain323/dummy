af2af1ba2554096000e9a1d3130675c931723aeb
sakibhossain323 <sakibhossain323@gmail.com>
2023-10-23 22:14:34 +0600

    console fix

--- src/console.cpp
+++ src/console.cpp
@@ -4,10 +4,13 @@ std::string Console::ReadLine()
 {
     char buffer[1000];
     fgets(buffer, 1000, stdin);
-    return std::string(buffer);
+    std::string content(buffer);
+    content.pop_back(); 
+    return content;
 }
 
 void Console::WriteLine(std::string content)
 {
+    content.push_back('\n');
     fputs(content.c_str(), stdout);
 }
